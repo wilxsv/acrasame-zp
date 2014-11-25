5f5973db27ee514497c1c611c1311bd2d78f363d
wilxsv <wilx.sv@gmail.com>
2014-11-25 10:17:26 -0600

    detalles de menu, eliminacion de lineas inecesarias

--- app/controllers/seguridad_controller.rb
+++ app/controllers/seguridad_controller.rb
@@ -1,9 +1,2 @@
 class SeguridadController < ApplicationController    
-  private
-    def require_login
-      unless logged_in?
-        flash[:error] = "Debe estar registrado para acceder a esta sección"
-        redirect_to home_index_path # halts request cycle
-      end
-    end
 end
